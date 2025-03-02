176a1b341c9396d795ffcf7bead29fa438b508c3
dayal chandra <dayalchandra511@gmail.com>
2025-03-02 11:48:03 +0600

    theme added

--- js/script.js
+++ js/script.js
@@ -208,3 +208,9 @@ document
 document.getElementById("discover").addEventListener("click", function () {
   window.location.href = "qna.html";
 });
+
+// THEME CHANGE
+document.getElementById("theme-btn").addEventListener("click", function () {
+  const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  document.body.style.backgroundColor = randomColor;
+});
